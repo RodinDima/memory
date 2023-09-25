1de05b7b58074857b86c23d947bd3e733629ba90
rodindima <rodindima1984@gmail.com>
2023-09-25 19:29:07 +0300

    2222

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,23 +1,24 @@
-import React from "react";
-
+import React, { useState } from "react";
 import Logo from "../Logo/Logo";
 
 import Button from "../Button/Button";
 
-import Sokol from "./Images/sokol.jpg";
-
 import { ReactComponent as FB } from "./Images/fb.svg";
+
+import { Hero } from "../Constants";
 
 import "./style.css";
 
-
-
 const MainPage = () => {
+  const [searchText, setSearchText] = useState("");
+  const filteredHeroes = Hero.filter((hero) =>
+    hero.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+
   return (
     <section className="main">
       <div className="main__container">
         <Logo />
-
         <div className="main__btns">
           <a href="#">
             <Button buttonText="Герої" />
@@ -30,90 +31,33 @@ const MainPage = () => {
           </a>
         </div>
         <form className="main__search">
-          <input type="text" placeholder="Введіть ім'я для пошуку..." />
+          <input
+            type="text"
+            value={searchText}
+            onChange={(e) => setSearchText(e.target.value)}
+            placeholder="Введіть ім'я для пошуку..."
+          />
         </form>
         <Button buttonText="Запропонувати" />
-        <div className="main__victim">
-          <div className="main__victim-item">
-            <a className="main__victim-link" href={`/person/1`}>
-              <img src={Sokol} alt="victim" />
-            </a>
+        {filteredHeroes.length > 0 ? (
+          <div className="main__victim">
+            {filteredHeroes.map((hero) => (
+              <div className="main__victim-item" key={hero.id}>
+                <a className="main__victim-link" href={`/person/${hero.id}`}>
+                  <img src={hero.photo} alt="victim" />
+                </a>
+              </div>
+            ))}
           </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href={`/person/2`}>
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-          <div className="main__victim-item">
-            <a className="main__victim-link" href="#">
-              <img src={Sokol} alt="victim" />
-            </a>
-          </div>
-        </div>
-       <div className="main__footer">
+        ) : (
+          <p>Нічого не знайдено</p>
+        )}
+        <div className="main__footer">
           <div>Поширити у</div>
-          <a className="main__link" href="#"><FB/></a>
-       </div>
+          <a className="main__link" href="#">
+            <FB />
+          </a>
+        </div>
       </div>
     </section>
   );
--- src/Components/Page1/Page1.jsx
+++ src/Components/Page1/Page1.jsx
@@ -4,25 +4,23 @@ import NameTemplate from "../NameTemplate/NameTemplate";
 import PositionTemplate from "../PositionTemplate/PositionTemplate";
 import BiographyTemplate from "../BiographyTemplate/BiographyTemplate";
 
-import { Hero, TEXT } from "../Constants";
+import { Hero } from "../Constants";
 
 import { useParams } from "react-router-dom";
 
-import Sokol from ".//Images/sokol.jpg";
 import Logo from "../Logo/Logo";
 import GloryTemplate from "../GloryTemplate/GloryTemplate";
 
 import "./style.css";
 
 const Page1 = () => {
-
-  const { id } = useParams();  
+  const { id } = useParams();
   const selectedHero = Hero.find((hero) => hero.id === parseInt(id));
 
-   if (!selectedHero) {
-     return null; 
-   }
-  
+  if (!selectedHero) {
+    return null;
+  }
+
   return (
     <section className="victim">
       <div className="victim__container">
