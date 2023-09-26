1a43a86bd810071b2930b82dee0d2d721cd76878
rodindima <rodindima1984@gmail.com>
2023-09-26 11:00:00 +0300

    222

--- src/Pages/Test/Test.jsx
+++ src/Pages/Test/Test.jsx
@@ -5,10 +5,13 @@ import "./style.css";
 const Test = () => {
     return (
       <div className="video__container">
+        <a href="/">
+          <h2 className="video__text">Шляхами війни</h2>
+        </a>
         <div className="video">
           <iframe
-            width="560"
-            height="315"
+            width="720"
+            height="405"
             src="https://www.youtube.com/embed/yrc7BqjuxYE?si=hrPqzFMni9jMsYtN"
             title="YouTube video player"
             frameborder="0"
@@ -16,9 +19,27 @@ const Test = () => {
             allowfullscreen
           ></iframe>
           <iframe
-            width="560"
-            height="315"
+            width="720"
+            height="405"
             src="https://www.youtube.com/embed/wS2GYSp-guQ?si=kcgmyyzyVgCVSu0e"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            width="720"
+            height="405"
+            src="https://www.youtube.com/embed/Gc1uT-KvQ7w?si=h1GaJtDynOWna8gV"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
+          <iframe
+            width="720"
+            height="405"
+            src="https://www.youtube.com/embed/xkjwSvCYKJc?si=FiQsWUdHSD2Rwmty"
             title="YouTube video player"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
