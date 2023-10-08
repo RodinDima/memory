3c940428c5ef2197755055199a0c8c61ebaae37c
rodindima <rodindima1984@gmail.com>
2023-10-08 16:48:21 +0300

    data-storage

--- src/Pages/FinalForm/FinalForm.jsx
+++ src/Pages/FinalForm/FinalForm.jsx
@@ -1,14 +1,11 @@
 import React, { useState } from "react";
 import { Form, Field } from "react-final-form";
-
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-
 import uk from "date-fns/locale/uk";
-
-import "firebase/database";
-
-import "firebase/firestore";
+import firebase from "firebase/compat/app"; // Импортируйте Firebase
+import "firebase/compat/storage"; // Импортируйте Firebase Storage
+import "firebase/compat/database"; // Импортируйте Firebase Realtime Database
 import axios from "axios";
 import { Dialog } from "@headlessui/react";
 import "./style.css";
@@ -17,21 +14,82 @@ const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const FinalForm = () => {
   const [isFormSubmit, setIsFormSubmit] = useState(false);
-
-
-
   const [selectedDate, setSelectedDate] = useState(null);
   const [selectedDate2, setSelectedDate2] = useState(null);
 
   const handleSubmit = async (values) => {
-  const response = await axios.post(
-      "https://users-1c07d-default-rtdb.firebaseio.com/users.json",
-      values
-    );
+    // Инициализация Firebase
+    const firebaseConfig = {
+      apiKey: "AIzaSyDjZVhUNwRL-CNUqIO2g0jy0mioIuCRnIE",
+      authDomain: "memory-bd8d1.firebaseapp.com",
+      projectId: "memory-bd8d1",
+      storageBucket: "memory-bd8d1.appspot.com",
+      messagingSenderId: "356858396307",
+      appId: "1:356858396307:web:5f49916991f2ebb0d21766",
+      measurementId: "G-0PYJBTDWSS", // опционально, если вы используете Analytics
+    };
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
+    }
 
-    console.log("Дані успішно відправлено!", values);
+    // Получите ссылку на Firebase Storage
+    const storage = firebase.storage();
+    const storageRef = storage.ref();
 
-    setIsFormSubmit(true);
+    const user = {
+      firstName: values.firstName || "", // Значение из инпута или пустая строка
+      lastName: values.lastName || "", // Значение из инпута или пустая строка
+      surname: values.surname || "", // Значение из инпута или пустая строка
+      employed: values.employed || false, // Значение из чекбокса или false, если не выбран
+      notes: values.notes || "", // Значение из текстового поля или пустая строка
+    };
+
+    console.log(user);
+
+    // Получите файл из элемента input
+    const fileInput = document.getElementById("fileInput");
+    const file = fileInput.files[0];
+
+    // Если файл выбран, загрузите его в Firebase Storage и добавьте ссылку в JSON-объект
+    if (file) {
+      const imageName = `${values.firstName}_${values.lastName}_${values.surname}`;
+      const imageRef = storageRef.child(`gloryk/${imageName}`);
+
+      // Загрузите изображение в Firebase Storage
+      const uploadTask = imageRef.put(file);
+
+      // Обработка успешной загрузки изображения
+      uploadTask.then((snapshot) => {
+        console.log("Изображение успешно загружено!");
+
+        // Получите ссылку на загруженное изображение
+        imageRef.getDownloadURL().then((imageUrl) => {
+          user.image = imageUrl; // Добавьте ссылку на изображение в JSON-объект
+
+          // Отправьте JSON-объект с данными пользователя в Firebase Realtime Database
+          axios
+            .post(
+              "https://users-1c07d-default-rtdb.firebaseio.com/users.json",
+              user
+            )
+            .then((response) => {
+              console.log("Данные успешно отправлены!", user);
+              setIsFormSubmit(true);
+            });
+        });
+      });
+    } else {
+      // Если файл не выбран, отправьте только JSON-объект с данными пользователя в Firebase Realtime Database
+      axios
+        .post(
+          "https://users-1c07d-default-rtdb.firebaseio.com/users.json",
+          user
+        )
+        .then((response) => {
+          console.log("Данные успешно отправлены!", user);
+          setIsFormSubmit(true);
+        });
+    }
   };
 
   return (
@@ -49,14 +107,14 @@ const FinalForm = () => {
                   name="firstName"
                   component="input"
                   type="text"
-                  placeholder="Введіть прізвище"
+                  placeholder="Введіть ім'я"
                 />
 
                 <Field
                   name="lastName"
                   component="input"
                   type="text"
-                  placeholder="Введіть ім'я"
+                  placeholder="Введіть прізвище"
                 />
 
                 <Field
@@ -112,6 +170,7 @@ const FinalForm = () => {
                   component="input"
                   type="file"
                   accept="image/*"
+                  id="fileInput"
                 />
               </div>
               <div className="buttons">
