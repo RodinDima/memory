e6b996717c3f265330e7047699e4277bd5278d97
rodindima <rodindima1984@gmail.com>
2024-02-07 11:59:47 +0200

    add-google-ads

--- src/Pages/HelpProj/HelpProj.jsx
+++ src/Pages/HelpProj/HelpProj.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from "react";
 import "./style.css";
-import Logo from "../../Components/Logo/Logo";
-import Button from "../../Components/Button/Button";
 
 import { TiLink } from "react-icons/ti";
 import { AiOutlineCheck } from "react-icons/ai";
@@ -28,52 +26,54 @@ const HelpProj = () => {
   return (
     <section className="proj__container">
       <Header className="custom-header" />
-      <Logo />
-      <Link to="/">
-        <GoArrowLeft className="link" />
-      </Link>
-      <h3 className="proj__title">
-        Підтримайте портал пам’яті одним із наступних способів:
-      </h3>
-      <div className="proj__text">
-        <p>
-          Інформаційна підтримка: розкажіть про наш проєкт на своїй сторінці в
-          соціальній мережі, друзям або знайомим.
-        </p>
-        <p>
-          Поставте посилання на наш ресурс там, де це буде доречно.{" "}
-          <span
-            className="proj__link"
-            style={{ cursor: "pointer", alignItems: "center" }}
-            onClick={copyResourceLink}
-            title="Натисніть для копіювання посилання"
-          >
-            {isCopied ? (
-              <>
-                <AiOutlineCheck /> Посилання зкопійоване
-              </>
-            ) : (
-              <>
-                <TiLink /> https://vilnohirsk-memory.com
-              </>
-            )}
-          </span>
-        </p>
-
-        <p>
-          Тематична підтримка: світлини воїнів, загиблих у ході
-          російсько-української війни, фото меморіалів, пам‘ятників,
-          меморіальних знаків, особисту інформацію про Героїв, спогади про них
-          рідних, друзів, колег, побратимів надсилайте на нашу електронну
-          скриньку:<br></br>
-          <a href="mailto:memoryvilnohirsk2023@gmail.com">
-            memoryvilnohirsk2023@gmail.com
-          </a>
-        </p>
-        <p>
-          Будемо вдячні, якщо Ви знайшли помилки або невідповідності і
-          повідомили про це нам.
-        </p>
+      {/*<Logo />*/}
+      <div>
+        <Link to="/">
+          <GoArrowLeft className="link" />
+        </Link>
+        <h3 className="proj__title">
+          Підтримайте портал пам’яті одним із наступних способів:
+        </h3>
+        <div className="proj__text">
+          <p>
+            Інформаційна підтримка: розкажіть про наш проєкт на своїй сторінці в
+            соціальній мережі, друзям або знайомим.
+          </p>
+          <p>
+            Поставте посилання на наш ресурс там, де це буде доречно.{" "}
+            <span
+              className="proj__link"
+              style={{ cursor: "pointer", alignItems: "center" }}
+              onClick={copyResourceLink}
+              title="Натисніть для копіювання посилання"
+            >
+              {isCopied ? (
+                <>
+                  <AiOutlineCheck /> Посилання зкопійоване
+                </>
+              ) : (
+                <>
+                  <TiLink /> https://vilnohirsk-memory.com
+                </>
+              )}
+            </span>
+          </p>
+  
+          <p>
+            Тематична підтримка: світлини воїнів, загиблих у ході
+            російсько-української війни, фото меморіалів, пам‘ятників,
+            меморіальних знаків, особисту інформацію про Героїв, спогади про них
+            рідних, друзів, колег, побратимів надсилайте на нашу електронну
+            скриньку:<br></br>
+            <a href="mailto:memoryvilnohirsk2023@gmail.com">
+              memoryvilnohirsk2023@gmail.com
+            </a>
+          </p>
+          <p>
+            Будемо вдячні, якщо Ви знайшли помилки або невідповідності і
+            повідомили про це нам.
+          </p>
+        </div>
       </div>
     </section>
   );
