032eaa6c899bd311e475873dd382a7c8638650c7
rodindima <rodindima1984@gmail.com>
2024-03-18 09:14:50 +0200

    last-change

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -53,63 +53,65 @@ const MainPage = () => {
   }, [windowWidth, filteredHeroes]);
 
   return (
-    <section className="main">
-      <div className="main__container">
-        <Header className="main__header" />
-        <WarInfo />
-        <TypingAnimation />
-        <Anniversary Hero={Hero} />
-        <div className="main__btns">
-          <Link to="/Help">
-            <Button
-              className="main__btns-help"
-              buttonText="Допомога родинам зниклих безвісті"
+    <div className="background-container">
+      <section className="main">
+        <div className="main__container">
+          <Header className="main__header" />
+          <WarInfo />
+          <TypingAnimation />
+          <Anniversary Hero={Hero} />
+          <div className="main__btns">
+            <Link to="/Help">
+              <Button
+                className="main__btns-help"
+                buttonText="Допомога родинам зниклих безвісті"
+              />
+            </Link>
+            <Link to="/test">
+              <Button className="main__btns-war" buttonText="Шляхами війни" />
+            </Link>
+          </div>
+          <form className="main__search">
+            <input
+              type="text"
+              value={searchText}
+              onChange={(e) => setSearchText(e.target.value)}
+              placeholder="Введіть ім'я для пошуку..."
             />
-          </Link>
-          <Link to="/test">
-            <Button className="main__btns-war" buttonText="Шляхами війни" />
-          </Link>
-        </div>
-        <form className="main__search">
-          <input
-            type="text"
-            value={searchText}
-            onChange={(e) => setSearchText(e.target.value)}
-            placeholder="Введіть ім'я для пошуку..."
-          />
-        </form>
-        <div className="main__victim">
-          {filteredHeroes.slice(0, displayedHeroes).map((hero) => (
-            <div className="main__victim-item" key={hero.id}>
-              <Link
-                className="main__victim-link"
-                to={`/person/${hero.id}`}
-                onClick={() => window.scrollTo(0, 0)}
-              >
-                <img src={hero.photo} alt="victim" />
-              </Link>
+          </form>
+          <div className="main__victim">
+            {filteredHeroes.slice(0, displayedHeroes).map((hero) => (
+              <div className="main__victim-item" key={hero.id}>
+                <Link
+                  className="main__victim-link"
+                  to={`/person/${hero.id}`}
+                  onClick={() => window.scrollTo(0, 0)}
+                >
+                  <img src={hero.photo} alt="victim" />
+                </Link>
 
-              <div className="main__victim-details">
-                <p>{hero.name}</p>
-                <p>{hero.date}</p>
+                <div className="main__victim-details">
+                  <p>{hero.name}</p>
+                  <p>{hero.date}</p>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
+          {filteredHeroes.length === 0 && (
+            <p className="nothing">Нікого не знайдено</p>
+          )}
+          {windowWidth < 575 && hasMoreHeroes && (
+            <Button
+              className="main__read-more-button"
+              buttonText="...далі"
+              handleMoreHeroes={showMoreHeroes}
+            />
+          )}
         </div>
-        {filteredHeroes.length === 0 && (
-          <p className="nothing">Нікого не знайдено</p>
-        )}
-        {windowWidth < 575 && hasMoreHeroes && (
-          <Button
-            className="main__read-more-button"
-            buttonText="...далі"
-            handleMoreHeroes={showMoreHeroes}
-          />
-        )}
-      </div>
-      <Footer />
-      <ScrollToTop />
-    </section>
+        <Footer />
+        <ScrollToTop />
+      </section>
+    </div>
   );
 };
 
